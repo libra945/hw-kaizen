05a53920d26b0696d16f970761243197f95b4404
libra945 <libra945@gmail.com>
2025-07-29 15:51:06 +0800

    增加驗證查詢筆數

--- tests/Report/SatisfactionPerformance.spec.js
+++ tests/Report/SatisfactionPerformance.spec.js
@@ -24,6 +24,7 @@ test('test', async ({ page }) => {
     });
   });
 
+
   // 原本操作流程
   await page.goto('https://web2023091801.azurewebsites.net/sso/Login');
   await page.getByRole('textbox', { name: '請輸入LDAP帳號' }).click();
@@ -33,7 +34,13 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: '相關報表' }).hover(); // mouse-move-on showing function list.
   await page.waitForTimeout(200); // wait 0.2 sec. for displaying list
   await page.getByRole('link', { name: '客戶滿意度查訪績效月報表' }).click();
-  await page.getByLabel('中心').selectOption('23');
+  // await page.getByLabel('中心').selectOption('23');
   await page.locator('#ddlDateRange').selectOption('Custom');
   await page.getByRole('button', { name: '查詢' }).click();
+
+  
+  // 等待表格資料載入，並驗證資料列筆數是否為 19 筆
+  await page.waitForTimeout(2000); // wait 2 sec. for displaying response rows.
+  const rows = page.locator('#perfTable tbody tr');
+  await expect(rows).toHaveCount(19);
 });
